a6f2a8c6ac3a8ad77cf82d42d2c5bb064eb8d4e9
danie <danie@10.10.12.124>
2022-10-30 12:51:54 +0200

    hii

--- ThreadCheckArray/src/ThreadCheckArray.java
+++ ThreadCheckArray/src/ThreadCheckArray.java
@@ -5,13 +5,8 @@ public class ThreadCheckArray implements Runnable
 	private boolean flag;
 	private boolean [] winArray;
 	SharedData sd;
-	ArrayList<Integer> array; //**
+	ArrayList<Integer> array; //** Set up variables
 	int b;
-	
-	
-	
-	
-	
 	/**
 	 * @param sd
 	 */
@@ -23,7 +18,7 @@ public class ThreadCheckArray implements Runnable
 			array = sd.getArray();
 			b = sd.getB();
 		}		
-		winArray = new boolean[array.size()]; //**
+		winArray = new boolean[array.size()]; //** Build a new array type boolean size of the array list
 	}
 	
 	void rec(int n, int b)
@@ -35,7 +30,8 @@ public class ThreadCheckArray implements Runnable
 		}	
 		if (n == 1)
 		{
-			if(b == 0 || b == array.get(n-1)) // **
+			if(b == 0 || b == array.get(n-1)) // ** Checks if the integer b can be calculated 
+				//** by the variables in the array list return true if it can be done else false
 			{
 				flag = true;
 				synchronized (sd) 
@@ -43,12 +39,12 @@ public class ThreadCheckArray implements Runnable
 					sd.setFlag(true);
 				}			
 			}
-			if (b == array.get(n-1)) // **
-				winArray[n-1] = true;
+			if (b == array.get(n-1)) // ** If b equals to the last elemnt in the array list
+				winArray[n-1] = true;//** the boolean array in the n-1 cell turn to true
 			return;
 		}
 		
-		rec(n-1, b - array.get(n-1)); // **
+		rec(n-1, b - array.get(n-1)); // ** Equation to check if b can be calculated by the elements 
 		if (flag)
 			winArray[n-1] = true;
 		synchronized (sd) 
@@ -62,11 +58,11 @@ public class ThreadCheckArray implements Runnable
 	public void run() {
 		if (array.size() != 1)
 			if (Thread.currentThread().getName().equals("thread1"))
-				rec(array.size()-1, b - array.get(array.size()-1)); // **
+				rec(array.size()-1, b - array.get(array.size()-1)); // **Equation to check if b can be calculated by the elements
 			else 
-				rec(array.size()-1, b); // **
-		if (array.size() == 1)  // **
-			if (b == array.get(0) && !flag) // **
+				rec(array.size()-1, b); // ** Thread2 takes the action here 
+		if (array.size() == 1)  // ** In case the array list has only one element
+			if (b == array.get(0) && !flag) // ** If b equals this elment change the boolean array in cell 0 to true
 			{
 				winArray[0] = true;
 				flag = true;
@@ -78,7 +74,7 @@ public class ThreadCheckArray implements Runnable
 		if (flag)
 		{
 			if (Thread.currentThread().getName().equals("thread1"))
-				winArray[array.size() - 1] = true; // **
+				winArray[array.size() - 1] = true; // ** checks if thread 1 is doing the action and return true
 			synchronized (sd) 
 			{
 				sd.setWinArray(winArray);
